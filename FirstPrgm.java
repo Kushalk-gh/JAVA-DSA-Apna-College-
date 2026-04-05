f9f039d327c288bc162138075928fdc0230d55bc
Kushal <kushalk1205@gmail.com>
2026-04-05 14:32:54 +0530

    first commit of simple * pattern using systemoutprintln

---
+++ FirstPrgm.java
@@ -0,0 +1,8 @@
+public class FirstPrgm {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
